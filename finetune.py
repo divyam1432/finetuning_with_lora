1035eca66e819d305eaeae23e8231682f7fefe25
Divyam Vashishth <divyamvashishth@Divyams-MacBook-Air.local>
2025-10-14 12:13:53 +0530

    add utility to perform finetuning using adapters.

--- finetune.py
+++ finetune.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.metrics import accuracy_score, f1_score
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, EvalPrediction, AutoTokenizer
 from peft import LoraConfig, get_peft_model, TaskType
-from adapters import AutoAdapterModel, AdapterTrainer
+# from adapters import AutoAdapterModel, AdapterTrainer
 
 import setup
 import dataset
