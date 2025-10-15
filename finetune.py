e84e9006a96cedfb403ef8fd6fd48eb5278654a2
Divyam Vashishth <divyamvashishth@Divyams-MacBook-Air.local>
2025-10-15 17:35:15 +0530

    add utility to perform finetuning using adapters. Fix import adapters issue 2

--- finetune.py
+++ finetune.py
@@ -11,7 +11,7 @@ import dataset
 from sklearn.metrics import accuracy_score, f1_score
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, EvalPrediction, AutoTokenizer
 from peft import LoraConfig, get_peft_model, TaskType
-# from adapters import AutoAdapterModel, AdapterTrainer
+from adapters import AutoAdapterModel, AdapterTrainer
 
 
 MODEL_NAME = "distilbert-base-uncased"
